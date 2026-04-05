5aaf531cd7649c02414e408f97f422802c0f42de
Sriram <jsri99@gmail.com>
2026-04-05 22:11:16 +0530

    Update SFLoginPage.ts

--- pages/SFLoginPage.ts
+++ pages/SFLoginPage.ts
@@ -16,11 +16,11 @@ export class SFLoginPage {
         await this.sfpage.getByRole("textbox", { name: "username" }).fill(LoginDetail.userName)
         const buffer=Buffer.from(LoginDetail.Password, 'base64')
         const decodedPassword=buffer.toString('utf-8')
-        console.log(decodedPassword)
+        //console.log(decodedPassword)
         await this.sfpage.getByRole("textbox", { name: "Password" }).fill(decodedPassword)
         await this.sfpage.getByRole("button", { name: "Log In" }).click()
         await this.sfpage.waitForTimeout(20000)
         await this.sfpage.context().storageState({ path: "Helper/TL_loginUser1.json" })
-       
+       //
     }
 }
