ca45b690e313ab2a30b6296e00f28ce2961c1ec5
Sriram <jsri99@gmail.com>
2026-04-11 10:51:35 +0530

    changes from user 2

--- tests/SFLogin.spec.ts
+++ tests/SFLogin.spec.ts
@@ -2,4 +2,4 @@ import {test} from "../Custom Fixture/SFCustomFixture"
 test ("CreateTask",async({SFLogin,SFHome,SFTask})=>{
     await SFLogin.login()
 })
-//check for user 1
+//Checkin from user 2.2
